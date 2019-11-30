f4a028202d2df7823ba25037e9d087790b399a0e
zain <54794958+zainalhendawe@users.noreply.github.com>
2019-11-30 04:29:10 -0800

    upload turo prj
    upload turo prj

---
+++ js/plugin.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    margin: 10,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 5
+        },
+        1000: {
+            items: 5
+        }
+    }
+});
+
+
+$(function() {
+    $('#basicExample').timepicker({
+      // options here
+    });
+  });
+  $(function () {
+    $('#datetimepicker5').datetimepicker();
+});
+
